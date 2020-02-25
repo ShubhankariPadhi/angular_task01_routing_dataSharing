e6ed5d4706b9be8eb4e21cb8cee0c26ddce8e3bf
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-02-25 19:29:00 +0530

    "Template Driven Forms and ReactiveFrom "

--- RoutingDataSharingProj/src/RegistrationFormDir/registration-form/registration-form.component.ts
+++ RoutingDataSharingProj/src/RegistrationFormDir/registration-form/registration-form.component.ts
@@ -7,11 +7,24 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./registration-form.component.scss']
 })
 export class RegistrationFormComponent implements OnInit {
-  firstnameFRomLocal;
+  /*created one object of any type and stored tdf element data in that obj in tepmlate for interpolation*/
+   model:any={};
+  specialities=['energetic','powerful','intelligent'];
+  // Reactive Form
+    registrationForm= new FormGroup({
+    firstName:new FormControl('',[Validators.required,Validators.minLength(3)]),
+    lastName:new FormControl('',Validators.required),
+    date:new FormControl('',Validators.required),
+    gender:new FormControl('',Validators.required),
+    emailId:new FormControl('',Validators.required),
+    password:new FormControl('',Validators.required),
+    confirmPassword:new FormControl('',Validators.required),
 
-  registrationForm= new FormGroup({
-    firstName:new FormControl('',Validators.required),
-    lastName:new FormControl('',Validators.required)
+    marks:new FormGroup({
+          math:new FormControl('',Validators.required),
+          english:new FormControl('',Validators.required),
+    }),
+
   });
   constructor(private fb: FormBuilder) {
 
@@ -19,11 +32,14 @@ export class RegistrationFormComponent implements OnInit {
 
   ngOnInit() {
   }
-onSubmit(){
+  onSubmit(){
+    console.log(JSON.stringify(this.registrationForm.value));
+  }
+
+  /*register(formData){
 //set local storage
-  this.firstnameFRomLocal= localStorage.setItem('registrationData', this.registrationForm.value);
-  console.warn(this.registrationForm.value);
+  // console.warn(this.registrationForm.value);
 
-
-}
+console.log(formData.value);
+}*/
 }
